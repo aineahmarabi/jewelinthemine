e66bb5280cf67411c6fac93ca6d11283e446b861
Aineah Marabi <aineahbwire@gmail.com>
2025-07-28 09:20:23 +0300

    feat: allow images within post body in Sanity post schema

--- studio/schemaTypes/post.ts
+++ studio/schemaTypes/post.ts
@@ -46,7 +46,20 @@ export default defineType({
       name: 'body',
       title: 'Body',
       type: 'array',
-      of: [{ type: 'block' }],
+      of: [
+        { type: 'block' },
+        {
+          type: 'image',
+          options: { hotspot: true },
+          fields: [
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative text',
+            },
+          ],
+        },
+      ],
     },
     {
       name: 'author',
